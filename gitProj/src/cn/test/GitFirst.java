a305299e00d519601e1c51b6c121595668214647
DaPeng <DaPeng@DaPeng-PC>
2017-03-17 15:51:53 +0800

    测试

---
+++ gitProj/src/cn/test/GitFirst.java
@@ -0,0 +1,13 @@
+package cn.test;
+
+public class GitFirst {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello! Git");
+	}
+
+}
